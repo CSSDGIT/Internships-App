0d4a12c2c602ce9389ee337a6250666adf83515b
Steven Tra <steven.tra6@gmail.com>
2017-11-03 18:29:22 -0400

    first heroku instance

--- app.js
+++ app.js
@@ -10,3 +10,28 @@ const app = express()
 
 // Sets the port for the app 
 app.set('port', (process.env.port || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json (JSON = JavaScript Object Notation)
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+	res.send('Chatbot up and running!')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+	// Let's make the verify token random to enforce security
+	if (req.query['hub.verify_token'] === 'dUD2dYajVZ') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+})
